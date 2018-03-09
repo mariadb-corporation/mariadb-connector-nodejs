83bafc15f98177658aa7c3c5148c2214c6947170
rusher <diego.dupin@gmail.com>
2018-03-09 18:44:10 +0100

    correct test

--- test/integration/test-timeout.js
+++ test/integration/test-timeout.js
@@ -9,9 +9,8 @@ describe("connection timeout", () => {
     const conn = base.createConnection({ host: "www.google.fr", connectTimeout: 1000 });
     conn.on("error", err => {
       assert.strictEqual(err.message, "(conn=-1) Connection timeout");
-      console.log(Date.now() - initTime);
-      assert.isTrue(Date.now() - initTime > 1000);
-      assert.isTrue(Date.now() - initTime < 1010);
+      assert.isTrue(Date.now() - initTime >= 1000);
+      assert.isTrue(Date.now() - initTime < 1050);
       done();
     });
   });
