413402a9bbf65701f353cd730f797f479e5dcc8a
rusher <diego.dupin@gmail.com>
2018-04-19 11:36:50 +0200

    display len on appveyor testing

--- test/conf.js
+++ test/conf.js
@@ -4,8 +4,7 @@ let baseConfig = {
   user: "root",
   database: "testn",
   host: "localhost",
-  port: 3306,
-  debugLen:16
+  port: 3306
 };
 
 if (process.env.TEST_HOST) baseConfig["host"] = process.env.TEST_HOST;
@@ -15,5 +14,6 @@ if (process.env.TEST_PASSWORD) baseConfig["password"] = process.env.TEST_PASSWOR
 if (process.env.TEST_DB) baseConfig["database"] = process.env.TEST_DB;
 if (process.env.TEST_PORT) baseConfig["port"] = parseInt(process.env.TEST_PORT, 10);
 if (process.env.TEST_SOCKET_PATH) baseConfig["socketPath"] = process.env.TEST_SOCKET_PATH;
+if (process.env.TEST_DEBUG_LEN) baseConfig["debugLen"] = process.env.TEST_DEBUG_LEN;
 
 module.exports.baseConfig = baseConfig;
