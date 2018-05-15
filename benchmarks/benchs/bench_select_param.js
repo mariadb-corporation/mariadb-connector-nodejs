12bb9d1302dc6251b5dbda5ca8b88373e6e88d70
rusher <diego.dupin@gmail.com>
2018-05-16 01:10:15 +0200

    [misc] small performance improvement, avoiding checking remaining buffer size before copying quote mainly

--- benchmarks/benchs/bench_select_param.js
+++ benchmarks/benchs/bench_select_param.js
@@ -4,7 +4,7 @@ module.exports.title = "select ?";
 module.exports.displaySql = "select ?";
 
 module.exports.benchFct = function(conn, deferred) {
-  const rand = Math.floor(Math.random() * 50000000);
+  const rand = "" + Math.floor(Math.random() * 50000000);
   conn.query("select ? as t", [rand], function(err, rows) {
     assert.ifError(err);
     assert.equal(rand, rows[0].t);
