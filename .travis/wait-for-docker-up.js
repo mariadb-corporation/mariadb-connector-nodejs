8384d9522e7ee3643858bd36525d90f1a90c3d4e
rusher <diego.dupin@gmail.com>
2018-06-07 18:13:42 +0200

    testing : updating server waiter script with promise implementation

--- .travis/wait-for-docker-up.js
+++ .travis/wait-for-docker-up.js
@@ -1,29 +1,26 @@
 "use strict";
 
-const Connection = require("../lib/connection");
-const ConnOptions = require("../lib/config/connection-options");
+const basePromise = require("../index");
 const Conf = require('../test/conf');
 
-const connOptions = new ConnOptions(Conf.baseConfig);
 let decrement = 30;
-const callback = () => console.log("docker db server up");
+
 const checkConnection = function() {
   decrement-=1;
 
-  let conn = new Connection(connOptions);
-  conn.connect((err) => {
-    if (err) {
-      console.error("Error connecting docker server (connection try " + (30 - decrement) + " of 30)");
-      if (decrement === 0) {
-        throw err;
-      } else {
-        setTimeout(checkConnection, 1000);
-      }
-    } else {
+  basePromise.createConnection(Conf.baseConfig)
+    .then((conn) => {
       conn.end();
-      callback();
-    }
-  });
-
+      console.log("docker db server up");
+    })
+    .catch((err) => {
+        console.error("Error connecting docker server (connection try " + (30 - decrement) + " of 30)");
+        if (decrement === 0) {
+          throw err;
+        } else {
+          setTimeout(checkConnection, 1000);
+        }
+      }
+    )
 };
 checkConnection();
