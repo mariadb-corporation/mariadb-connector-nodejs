0052ecde6b9c7effd5702c587a860c25a0845986
rusher <diego.dupin@gmail.com>
2018-06-18 12:52:03 +0200

    adding new tests

---
+++ test/integration/test-compression.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const base = require("../base.js");
+const { assert } = require("chai");
+
+describe("Compression", function() {
+  const testSize = 16 * 1024 * 1024 + 800; // more than one packet
+  let maxAllowedSize, buf;
+  let conn;
+
+  before(function(done) {
+    base
+      .createConnection({ compress: true, multipleStatements: true })
+      .then(con => {
+        conn = con;
+        conn.query("SELECT @@max_allowed_packet as t").then(row => {
+          maxAllowedSize = row[0].t;
+          if (testSize < maxAllowedSize) {
+            buf = Buffer.alloc(testSize);
+            for (let i = 0; i < buf.length; i++) {
+              buf[i] = 97 + (i % 10);
+            }
+          }
+          done();
+        });
+      })
+      .catch(done);
+  });
+
+  after(function(done) {
+    conn
+      .end()
+      .then(() => {
+        done();
+      })
+      .catch(done);
+  });
+
+  it("simple select 1", function(done) {
+    conn
+      .query("SELECT 1")
+      .then(rows => {
+        assert.deepEqual(rows, [{ "1": 1 }]);
+        done();
+      })
+      .catch(done);
+  });
+
+  it("multiple packet result (multiple rows)", function(done) {
+    //using sequence engine
+    if (!conn.isMariaDB() || !conn.hasMinVersion(10, 1)) this.skip();
+    conn
+      .query("select 1; DO 1;select 2")
+      .then(rows => {
+        assert.equal(rows.length, 3);
+        assert.deepEqual(rows[0], [{ "1": 1 }]);
+        assert.deepEqual(rows[1], { affectedRows: 0, insertId: 0, warningStatus: 0 });
+        assert.deepEqual(rows[2], [{ "2": 2 }]);
+        done();
+      })
+      .catch(done);
+  });
+
+  it("parameter bigger than 16M packet size", function(done) {
+    if (maxAllowedSize <= testSize) this.skip();
+    this.timeout(10000); //can take some time
+    conn.query("CREATE TEMPORARY TABLE bigParameter (b longblob)");
+    conn
+      .query("insert into bigParameter(b) values(?)", [buf])
+      .then(() => {
+        return conn.query("SELECT * from bigParameter");
+      })
+      .then(rows => {
+        assert.deepEqual(rows[0].b, buf);
+        done();
+      })
+      .catch(done);
+  });
+});
---
+++ test/integration/test-connection-opts.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const base = require("../base.js");
+const { assert } = require("chai");
+
+describe("connection option", () => {
+  it("with undefined collation", function(done) {
+    base
+      .createConnection({ charset: "unknown" })
+      .then(() => {
+        done(new Error("must have thrown error!"));
+      })
+      .catch(err => {
+        assert.isTrue(err.message.includes("Unknown charset"));
+        done();
+      });
+  });
+
+  it("timezone Z", function(done) {
+    base
+      .createConnection({ timezone: "Z" })
+      .then(conn => {
+        conn.query("SET SESSION time_zone = '+01:00'");
+        conn
+          .query("SELECT UNIX_TIMESTAMP(?) tt", [new Date("2000-01-01T00:00:00Z")])
+          .then(res => {
+            assert.deepEqual(res[0].tt, 946681200);
+            conn.end();
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+
+
+  it("timezone +2h", function(done) {
+    base
+      .createConnection({ timezone: "+02" })
+      .then(conn => {
+        conn.query("SET SESSION time_zone = '+01:00'");
+        conn
+          .query("SELECT UNIX_TIMESTAMP(?) tt", [new Date("2000-01-01T00:00:00Z")])
+          .then(res => {
+            assert.deepEqual(res[0].tt, 946688400);
+            conn.end();
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+
+  it("timezone +2h00", function(done) {
+    base
+      .createConnection({ timezone: "+02:00" })
+      .then(conn => {
+        conn.query("SET SESSION time_zone = '+01:00'");
+        conn
+          .query("SELECT UNIX_TIMESTAMP(?) tt", [new Date("2000-01-01T00:00:00Z")])
+          .then(res => {
+            assert.deepEqual(res[0].tt, 946688400);
+            conn.end();
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+
+  it("timezone +1h", function(done) {
+    base
+      .createConnection({ timezone: "+01:00" })
+      .then(conn => {
+        conn.query("SET SESSION time_zone = '+01:00'");
+        conn
+          .query("SELECT UNIX_TIMESTAMP(?) tt", [new Date("2000-01-01T00:00:00+0100")])
+          .then(res => {
+            assert.deepEqual(res[0].tt, 946681200);
+            conn.end();
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+
+  it("wrong timezone format", function(done) {
+    base
+      .createConnection({ timezone: "+e:00" })
+      .then(conn => {
+        done(new Error("Must have thrown exception"));
+      })
+      .catch(err => {
+        assert.isTrue(err.message.includes("timezone format error"));
+        done();
+      });
+  });
+
+  it("nestTables results", function(done) {
+    base
+      .createConnection({ nestTables: true })
+      .then(conn => {
+        conn.query("CREATE TEMPORARY TABLE t1 (a varchar(20))");
+        conn.query("CREATE TEMPORARY TABLE t2 (b varchar(20))");
+        conn.query("INSERT INTO t1 VALUES ('bla'), ('bla2')");
+        conn.query("INSERT INTO t2 VALUES ('bou')");
+        conn
+          .query("SELECT * FROM t1, t2")
+          .then(rows => {
+            assert.deepEqual(rows, [
+              { t1: { a: "bla" }, t2: { b: "bou" } },
+              { t1: { a: "bla2" }, t2: { b: "bou" } }
+            ]);
+            conn.end();
+            done();
+          })
+          .catch(done);
+      })
+      .catch(done);
+  });
+});
