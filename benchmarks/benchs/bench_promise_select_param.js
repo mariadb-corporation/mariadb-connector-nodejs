0fc63829b139ef7b4d5d630ee58ec12036c2893c
rusher <diego.dupin@gmail.com>
2018-07-05 17:27:14 +0200

    adding benchmark tests

--- benchmarks/benchs/bench_promise_select_param.js
+++ benchmarks/benchs/bench_promise_select_param.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 
-module.exports.title = "select random number using promise";
-module.exports.displaySql = "select ?";
+module.exports.title = "select number using promise";
+module.exports.displaySql = "select 10000000";
 module.exports.promise = true;
 module.exports.benchFct = function(conn, deferred) {
   conn
