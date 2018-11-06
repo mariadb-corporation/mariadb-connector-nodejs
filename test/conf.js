65c1ad2be2b5f6af585b8a0e1322a38c8e0ca2eb
rusher <diego.dupin@gmail.com>
2018-11-06 15:00:28 +0100

    [misc] add debug option logPackets permitting to log last packets to error

--- test/conf.js
+++ test/conf.js
@@ -15,5 +15,6 @@ if (process.env.TEST_DB) baseConfig["database"] = process.env.TEST_DB;
 if (process.env.TEST_PORT) baseConfig["port"] = parseInt(process.env.TEST_PORT, 10);
 if (process.env.TEST_SOCKET_PATH) baseConfig["socketPath"] = process.env.TEST_SOCKET_PATH;
 if (process.env.TEST_DEBUG_LEN) baseConfig["debugLen"] = process.env.TEST_DEBUG_LEN;
+if (process.env.TEST_LOG_PACKETS) baseConfig["logPackets"] = true;
 
 module.exports.baseConfig = baseConfig;
