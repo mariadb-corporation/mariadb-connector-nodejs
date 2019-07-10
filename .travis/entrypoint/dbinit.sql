76fb12511cf44d0a8312a6fe74a9e5923719470e
rusher <diego.dupin@gmail.com>
2019-07-10 12:39:15 +0200

    [misc] maxscale testing 10 4 bases

--- .travis/entrypoint/dbinit.sql
+++ .travis/entrypoint/dbinit.sql
@@ -1,7 +1,7 @@
 CREATE USER 'bob'@'%';
 GRANT ALL ON *.* TO 'bob'@'%' with grant option;
 
-CREATE USER 'boby'@'%' identified by 'hey';
+CREATE USER 'boby'@'%' identified by 'heyPassw0@rd';
 GRANT ALL ON *.* TO 'boby'@'%' with grant option;
 
 INSTALL PLUGIN pam SONAME 'auth_pam';
