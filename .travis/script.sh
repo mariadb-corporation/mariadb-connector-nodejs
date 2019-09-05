6ae18cc86b32723bfbdbd1d4dcfbc60596c498b0
rusher <diego.dupin@gmail.com>
2019-09-05 10:51:15 +0200

    [misc] removing node.js 6 from CI, ESLint 6 requiring 8 or newer

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ else
   docker-compose -f .travis/docker-compose.yml up -d
 fi
 
-if [ -z "$SKIP_LEAK" ] ; then npm install node-memwatch; fi
+npm install node-memwatch
 
 node .travis/wait-for-docker-up.js
 
