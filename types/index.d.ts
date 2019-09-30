d729a65db23e12fe96cf48817e1aa48a265c2926
rusher <diego.dupin@gmail.com>
2019-09-30 12:17:13 +0200

    [CONJS-98] Adding missing 'collation' option in Typescript definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -133,11 +133,21 @@ export interface UserConnectionConfig {
   connectAttributes?: any;
 
   /**
-   * The charset for the connection. This is called "collation" in the SQL-level of MySQL (like utf8_general_ci).
-   * If a SQL-level charset is specified (like utf8mb4) then the default collation for that charset is used.
-   * (Default: 'UTF8MB4_UNICODE_CI')
+   * Protocol character set used with the server.
+   * Connection collation will be the default collation associated with charset.
+   * It's mainly used for micro-optimizations. The default is often sufficient.
+   * example 'UTF8MB4', 'CP1250'.
+   * (default 'UTF8MB4')
    */
   charset?: string;
+
+  /**
+   * Permit to defined collation used for connection.
+   * This will defined the charset encoding used for exchanges with database and defines the order used when
+   * comparing strings. It's mainly used for micro-optimizations
+   * (Default: 'UTF8MB4_UNICODE_CI')
+   */
+  collation?: string;
 
   /**
    * The MySQL user to authenticate as
