8c70fa69277cda950ff90c7cfe7d3c9431ec2b57
rusher <diego.dupin@gmail.com>
2020-10-15 14:14:13 +0200

    [misc] appveyor correction
    retrieving server msi file follow redirection

--- appveyor-download.bat
+++ appveyor-download.bat
@@ -2,12 +2,11 @@
 set archive=http://ftp.hosteurope.de/mirror/archive.mariadb.org//mariadb-%DB%/winx64-packages/mariadb-%DB%-winx64.msi
 set last=http://mirror.i3d.net/pub/mariadb//mariadb-%DB%/winx64-packages/mariadb-%DB%-winx64.msi
 
-curl -fsS -o server.msi %archive% 
+curl -fLsS -o server.msi %archive%
 
 if %ERRORLEVEL% == 0 goto end
 
-
-curl -fsS -o server.msi %last%
+curl -fLsS -o server.msi %last%
 if %ERRORLEVEL% == 0  goto end
 
 echo Failure Reason Given is %errorlevel%
