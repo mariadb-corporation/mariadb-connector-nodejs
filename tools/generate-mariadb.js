10c76605677116b9076154b69112a5e2246c2145
diego Dupin <diego.dupin@mariadb.com>
2022-07-25 09:30:05 +0200

    [CONJS-213] update error codes with recent mariadb version

--- tools/generate-mariadb.js
+++ tools/generate-mariadb.js
@@ -11,7 +11,7 @@ const readline = require('readline');
 const os = require('os');
 const path = require('path');
 
-const version = '10.5';
+const version = '10.9';
 const extendedUrl = 'https://raw.githubusercontent.com/MariaDB/server/' + version + '/sql/share/errmsg-utf8.txt';
 const baseUrl = 'https://raw.githubusercontent.com/MariaDB/server/' + version + '/include/my_base.h';
 const fileName = path.join(os.tmpdir(), 'mariadb_errmsg.txt');
