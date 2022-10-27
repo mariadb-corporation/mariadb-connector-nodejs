d2e626e41b48e80301b0e867a922843520e7f7c5
diego <diego.dupin@gmail.com>
2022-10-27 18:46:32 +0200

    [misc] ensure prepare cache use counter

---
+++ benchmarks/benchs/select_1000_rows_execute.js
@@ -0,0 +1,20 @@
+module.exports.title = 'select 1000 rows';
+module.exports.displaySql = 'select * from 1000 rows (int + string(32))';
+module.exports.requireExecute = true;
+module.exports.benchFct = async function (conn, type, deferred) {
+  const res = await conn.execute('select * from 1000rows');
+  deferred.resolve(res);
+};
+
+module.exports.initFct = function (conn) {
+  conn.query('DROP TABLE IF EXISTS 1000rows');
+  conn.query('CREATE TABLE 1000rows(id INT SIGNED not null primary key auto_increment, val VARCHAR(32))');
+  let inserts = [];
+  for (let i = 0; i < 1000; i++) {
+    inserts.push(conn.query('INSERT INTO 1000rows(val) VALUES (?) ', ['abcdefghijabcdefghijabcdefghijaa']));
+  }
+  return Promise.all(inserts).catch((e) => {
+    console.log(e);
+    throw e;
+  });
+};
