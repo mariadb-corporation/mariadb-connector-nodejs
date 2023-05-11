32ce9df60ee8ff95efb425f55c2e9e7b4bcb6be6
rusher <diego.dupin@gmail.com>
2023-05-11 12:48:24 +0000

    [misc] correct wrong mysql RSA test

--- test/integration/test-auth-plugin.js
+++ test/integration/test-auth-plugin.js
@@ -399,13 +399,6 @@ describe('authentication plugin', () => {
 
   it('sha256 authentication plugin with public key retrieval', function (done) {
     if (shareConn.info.isMariaDB() || !shareConn.info.hasMinVersion(5, 7, 0)) this.skip();
-    // request files since 5.7.40 / 8.0.31 fails when requesting public key
-    if (
-      !shareConn.info.isMariaDB() &&
-      ((!shareConn.info.hasMinVersion(8, 0, 0) && shareConn.info.hasMinVersion(5, 7, 40)) ||
-        shareConn.info.hasMinVersion(8, 0, 31))
-    )
-      this.skip();
 
     const self = this;
     base
